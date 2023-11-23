e58357e93fe3c40ee947c3f5a55d8d0ce757d906
liutsar1337 <liutsar346@gmail.com>
2023-11-23 11:47:52 +0200

    Verify if user is authenticated before access to the page

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainScreen from '../components/MainScreen';
-import {store} from '../redux/store';
 import Login from '../components/Login';
 import Register from '../components/Register';
 import Profile from "../components/Profile";
@@ -12,19 +11,25 @@ import TestingWork from "../components/testingWork";
 import {useSelector} from "react-redux";
 
 const App = () => {
-    const user = useSelector(state => state.auth.user)
+    const isAuth = Boolean(useSelector(state => state.auth.user))
     return (
                 <Router>
                     <TopBar/>
-                    {JSON.stringify(store)}
                     <Routes>
                         <Route path="/" element={<MainScreen/>}/>
                         <Route path="/login" element={<Login/>}/>
                         <Route path="/landing" element={<LandingPage/>}/>
                         <Route path="/register" element={<Register/>}/>
-                        <Route path="/profile" element={<Profile/>}/>
-                        <Route path="/work" element={<Work/>}/>
-                        <Route path="/testingwork" element={<TestingWork/>}/> </Routes>
+                        <Route path="/profile" element={
+                            isAuth ? <Profile/> : <Login/>
+                        }/>
+                        <Route path="/work" element={
+                            isAuth ? <Work/> : <Login/>
+                        }/>
+                        <Route path="/testingwork" element={
+                            isAuth ? <TestingWork/> : <Login/>
+                        }/>
+                    </Routes>
                 </Router>
     )
 };
